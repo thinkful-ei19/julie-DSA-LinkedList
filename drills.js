2830c5d613718c5b45fd34f4fba404dd78e509d4
Julie K <julieskim160@gmail.com>
2018-05-17 08:48:05 -0700

    class node and start on linked list clas

--- drills.js
+++ drills.js
@@ -5,3 +5,27 @@ class _Node {
     }
 }
 
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+    insertFirst(item) {
+        this.head = new _Node(item, this.head);
+    }
+    insertLast(item) {
+        if(this.head === null) {
+            this.insertFirst(item);
+        } else {
+            let tempNode = this.head;
+            while(tempNode.next !== null) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = new _Node(item, null);
+        }
+        
+    }
+    remove()
+    find()
+
+   
+}
