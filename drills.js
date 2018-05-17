5f95b57be5c4b2c295d11f94c739615b44166bc6
Julie K <julieskim160@gmail.com>
2018-05-17 09:05:09 -0700

    SSL add items

--- drills.js
+++ drills.js
@@ -9,9 +9,11 @@ class LinkedList {
     constructor() {
         this.head = null
     }
+
     insertFirst(item) {
         this.head = new _Node(item, this.head);
     }
+
     insertLast(item) {
         if(this.head === null) {
             this.insertFirst(item);
@@ -24,8 +26,54 @@ class LinkedList {
         }
         
     }
-    remove()
-    find()
 
+    remove(item) {
+        if (!this.head){
+            return null;
+        }
+        if (this.head.value == item) {
+            this.head = this.head.mext;
+            return
+        }
+        let currNode = this.head;
+        let previousNode = this.head;
+
+        while ((currNode !== null) && (currNode.value !== item)) {
+            previousNode = currNode;
+            currNode = currNode.next;
+        }
+        if(currNode === null) {
+            console.log('Item not found');
+            reurn;
+        }
+        previousNode.next = currNode.next;
+    }
+
+    find(item) {
+        let currNode = this.head;
+        if(!this.head) {
+            return null;
+        }
+        while(currNode.value !== item) {
+            if (currNode.next == null) {
+                return null
+            } else {
+                currNode = currNode.next;
+            }
+        }
+        return currNode;
+    }
    
 }
+
+function main(){
+    let SSL = new LinkedList();
+        SSL.insertFirst('Apollo');
+        SSL.insertFirst('Boomer');
+        SSL.insertFirst('Helo');
+        SSL.insertFirst('Husker');
+        SSL.insertFirst('Starbuck');
+    console.log(SSL)
+}
+
+main()
